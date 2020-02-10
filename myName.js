a9b836a3cb67517da863d6cc08d8345119a5e058
MediocreIan <idrews094@gmail.com>
2020-02-10 14:58:59 -0600

    finished project

---
+++ myName.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function getYearOfBirth(age) {
+  if (age < 1) {
+    throw new Error('age cannot be negative');
+  } else {
+    return 2020 - age;
+  }
+}
+
+function createGreeting(name, age) {
+  if (age < 1) {
+    throw new Error('age cannot be negative');
+  }
+  if (name === undefined || age === undefined) {
+    throw new Error('age cannot be negative');
+  }
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number');
+  }
+  const yob = getYearOfBirth(age);
+  return `Hi my name is ${name} I was born in ${yob} I am ${age} years old`;
+}
+const greeting1 = createGreeting('Ian', 25);
+console.log(greeting1);
